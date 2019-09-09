fce135bf11e8364dd5408a8e7f92546eddbb7650
Albert Souza <34682279+ASOEstudio@users.noreply.github.com>
2019-09-09 16:38:07 -0300

    verificação de prefers-color-scheme media query

--- theme-scheme/scripts.js
+++ theme-scheme/scripts.js
@@ -1,25 +1,31 @@
-var hour = new Date().getHours();
-if (hour < 6 || hour >= 15) {
-    nightMode();
-    console.log('verificou o horário');
+const hour = new Date().getHours();
+const isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+const isLightMode = window.matchMedia('(prefers-color-scheme: light)').matches;
+const isNotSpecified = window.matchMedia('(prefers-color-scheme: no-preference)').matches;
+const hasNotSupport = !isDarkMode && !isLightMode && !isNotSpecified;
+
+console.log('verificando a Media Query "prefers-color-scheme": é dark = ', isDarkMode, ' é light = ', isLightMode, ' não é definido ', isNotSpecified, ' não é suportado ', hasNotSupport);
+
+if (hasNotSupport) {
+    if (hour < 6 || hour >= 18) nightMode();
+    // console.log('verificou o horário');
 
     /* caso o script estiver sendo executado antes do DOM ser montado
-    
+
     setTimeout(() => {
-        console.log('é mais de 15 horas')
         themeSchemeChange();
     }, 500);
     */
 }
 
-function themeSchemeChange() {
-    console.log('chegou na função')
-    var element = document.querySelector('body');
+function themeSchemeButton() {
+    console.log('função do botão')
+    const element = document.querySelector('body');
     element.classList.toggle('dark-mode');
 }
 
 function nightMode() {
-    console.log('chegou na função')
-    var element = document.querySelector('body');
+    console.log('função pelo horário')
+    const element = document.querySelector('body');
     element.classList.toggle('night-mode');
 }
