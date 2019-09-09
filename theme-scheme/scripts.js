b4ccf3908483e678441feb5dd039ae6a7b3d6402
Albert Souza <34682279+ASOEstudio@users.noreply.github.com>
2019-09-09 15:42:34 -0300

    verificando o horario para definir esquema de cores

--- theme-scheme/scripts.js
+++ theme-scheme/scripts.js
@@ -1,4 +1,25 @@
+var hour = new Date().getHours();
+if (hour < 6 || hour >= 15) {
+    nightMode();
+    console.log('verificou o horário');
+
+    /* caso o script estiver sendo executado antes do DOM ser montado
+    
+    setTimeout(() => {
+        console.log('é mais de 15 horas')
+        themeSchemeChange();
+    }, 500);
+    */
+}
+
 function themeSchemeChange() {
+    console.log('chegou na função')
     var element = document.querySelector('body');
-    element.classList.add("dark-mode");
+    element.classList.toggle('dark-mode');
+}
+
+function nightMode() {
+    console.log('chegou na função')
+    var element = document.querySelector('body');
+    element.classList.toggle('night-mode');
 }
