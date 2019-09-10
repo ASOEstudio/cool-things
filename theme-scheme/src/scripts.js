114f5c9602d4582212d2f650e9d33fe1f78392ae
Albert Souza <34682279+ASOEstudio@users.noreply.github.com>
2019-09-10 17:14:17 -0300

    testando es6 com babel, porem ainda não está funcionando como deveria

---
+++ theme-scheme/src/scripts.js
@@ -0,0 +1,41 @@
+export class Core {
+
+    constructor() {
+        hour;
+        hasNotSupport;
+        this.init();
+    }
+
+    init() {
+        this.hour = new Date().getHours();
+        const isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+        const isLightMode = window.matchMedia('(prefers-color-scheme: light)').matches;
+        const isNotSpecified = window.matchMedia('(prefers-color-scheme: no-preference)').matches;
+        this.hasNotSupport = !isDarkMode && !isLightMode && !isNotSpecified;
+        console.log('verificando a Media Query "prefers-color-scheme": é dark = ', isDarkMode, ' é light = ', isLightMode, ' não é definido ', isNotSpecified, ' não é suportado ', hasNotSupport);
+    
+        if (this.hasNotSupport || isNotSpecified) themeDesabled();
+    }
+    
+    themeDesabled() {
+        const element = document.getElementsByTagName('button')[0];
+        element.setAttribute('disabled', 'true');
+        console.log(element);
+        if (this.hour < 6 || this.hour >= 12) nightMode();
+    }
+    
+    themeSchemeButton() {
+        console.log('função do botão');
+        const element = document.querySelector(':root');
+        element.classList.toggle('dark-mode');
+        console.log(element);
+    }
+    
+    // navegadores que não suportam classList
+    nightMode() {
+        console.log('função pelo horário')
+        const element = document.getElementsByTagName('body')[0];
+        element.className += ' night-mode';
+        console.log(element);
+    }
+}
