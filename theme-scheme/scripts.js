ba4083516be31cac7974aaefc7d47fc7bc50ec89
Albert Souza <34682279+ASOEstudio@users.noreply.github.com>
2019-09-10 11:38:57 -0300

    corrindo js para funcionar com o IE

--- theme-scheme/scripts.js
+++ theme-scheme/scripts.js
@@ -1,31 +1,36 @@
-const hour = new Date().getHours();
-const isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
-const isLightMode = window.matchMedia('(prefers-color-scheme: light)').matches;
-const isNotSpecified = window.matchMedia('(prefers-color-scheme: no-preference)').matches;
-const hasNotSupport = !isDarkMode && !isLightMode && !isNotSpecified;
+init();
 
-console.log('verificando a Media Query "prefers-color-scheme": é dark = ', isDarkMode, ' é light = ', isLightMode, ' não é definido ', isNotSpecified, ' não é suportado ', hasNotSupport);
+function init() {
+    const hour = new Date().getHours();
+    const isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    const isLightMode = window.matchMedia('(prefers-color-scheme: light)').matches;
+    const isNotSpecified = window.matchMedia('(prefers-color-scheme: no-preference)').matches;
+    const hasNotSupport = !isDarkMode && !isLightMode && !isNotSpecified;
+    
+    console.log('verificando a Media Query "prefers-color-scheme": é dark = ', isDarkMode, ' é light = ', isLightMode, ' não é definido ', isNotSpecified, ' não é suportado ', hasNotSupport);
+    
+    if (hasNotSupport || isNotSpecified) {
+        if (hasNotSupport) {
+            const element = document.getElementsByTagName('button')[0];
+            element.setAttribute('disabled', 'true');
+            console.log(element);
+        }
 
-if (hasNotSupport) {
-    if (hour < 6 || hour >= 18) nightMode();
-    // console.log('verificou o horário');
-
-    /* caso o script estiver sendo executado antes do DOM ser montado
-
-    setTimeout(() => {
-        themeSchemeChange();
-    }, 500);
-    */
+        if (hour < 6 || hour >= 9) nightMode();
+    }
 }
 
 function themeSchemeButton() {
-    console.log('função do botão')
-    const element = document.querySelector('body');
+    console.log('função do botão');
+    const element = document.querySelector(':root');
     element.classList.toggle('dark-mode');
+    console.log(element);
 }
 
+// navegadores que não suportam classList
 function nightMode() {
     console.log('função pelo horário')
-    const element = document.querySelector('body');
-    element.classList.toggle('night-mode');
+    const element = document.getElementsByTagName('body')[0];
+    element.className += ' night-mode';
+    console.log(element);
 }
