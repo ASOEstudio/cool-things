3af7027a188e5059170c7ce4dafe2a6233f07916
Albert Souza <34682279+ASOEstudio@users.noreply.github.com>
2019-09-16 17:50:09 -0300

    ajustando interação do main quando top 0, deixando mais clean

--- in-scroll-hide-toolbar/scripts.js
+++ in-scroll-hide-toolbar/scripts.js
@@ -1,41 +1,36 @@
+'use strict';
+
 (function() {
-    'use strict';
 
     // setando elementos no DOM
     var elHeader = document.querySelector('header'),
         elMain = document.querySelector('main'),
-        elFooter = document.querySelector('footer');
+        elFooter = document.querySelector('footer'),
         elCopyFooter = document.querySelector('.footer__copy');
 
     // sai da função se o elemento não existir
-    if(!elHeader && !elMain && !elFooter) return true;
+    if(!elHeader && !elMain && !elFooter && !elCopyFooter) return true;
 
-    elMain.style.top = elHeader.clientHeight;
+    // elMain.style.top = elHeader.clientHeight;
     elCopyFooter.innerHTML = `copyright &copy; ${new Date().getFullYear()}`;
-    
-    console.log('altura do header', elHeader.clientHeight);
-    console.log('top main', elMain.style.top);
 
     // setando variáveis para scroll
-    // var docHeight = 0,
-    //     winHeight = 0,
-    var winScrollCurrent = 0,
+    var docHeight = 0,
+        winHeight = 0,
+        winScrollCurrent = 0,
         winScrollBefore = 0;
-        // winScrollDiff = 0,
 
     window.addEventListener('scroll', function() {
 
         // setando elementos no DOM
-        // docHeight = document.body.offsetHeight;
         docHeight = document.body.scrollHeight;
         winHeight = window.innerHeight;
         winScrollCurrent = window.pageYOffset;
         
-        console.log('docHeight', docHeight, 'winHeight', winHeight,
-        'winScrollCurrent', winScrollCurrent, 'winScrollBefore', winScrollBefore);
-        
         // scroll movido para baixo
         if (winScrollCurrent > 0) {
+            elHeader.classList.remove('header--page-top');
+            elMain.classList.add('main--page-top');
 
             // verifica se o scroll continua indo para baixo
             if (winScrollBefore < winScrollCurrent) {
@@ -61,7 +56,9 @@
         // scroll '0' no topo da página
         else {
             elHeader.classList.remove('header--hidden');
-            elHeader.classList.add('header--show');
+            elHeader.classList.add('header--show', 'header--page-top');
+
+            elMain.classList.remove('main--page-top');
         };
 
         // verifica se a página chegou ao final
