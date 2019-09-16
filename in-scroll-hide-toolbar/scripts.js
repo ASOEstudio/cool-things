cd3644f2d70ef93f1f8360902b7daa009c0529cd
Albert Souza <34682279+ASOEstudio@users.noreply.github.com>
2019-09-16 16:56:53 -0300

    finalizando foncionalidade com o footer

--- in-scroll-hide-toolbar/scripts.js
+++ in-scroll-hide-toolbar/scripts.js
@@ -4,13 +4,14 @@
     // setando elementos no DOM
     var elHeader = document.querySelector('header'),
         elMain = document.querySelector('main'),
-        elFooter = document.querySelector('.footer__copy');
+        elFooter = document.querySelector('footer');
+        elCopyFooter = document.querySelector('.footer__copy');
 
     // sai da função se o elemento não existir
     if(!elHeader && !elMain && !elFooter) return true;
 
     elMain.style.top = elHeader.clientHeight;
-    elFooter.innerHTML = `copyright &copy; ${new Date().getFullYear()}`
+    elCopyFooter.innerHTML = `copyright &copy; ${new Date().getFullYear()}`;
     
     console.log('altura do header', elHeader.clientHeight);
     console.log('top main', elMain.style.top);
@@ -26,11 +27,12 @@
 
         // setando elementos no DOM
         // docHeight = document.body.offsetHeight;
-        // winHeight = window.innerHeight;
+        docHeight = document.body.scrollHeight;
+        winHeight = window.innerHeight;
         winScrollCurrent = window.pageYOffset;
         
-        // console.log('docHeight', docHeight, 'winHeight', winHeight,
-        // 'winScrollCurrent', winScrollCurrent, 'winScrollBefore', winScrollBefore);
+        console.log('docHeight', docHeight, 'winHeight', winHeight,
+        'winScrollCurrent', winScrollCurrent, 'winScrollBefore', winScrollBefore);
         
         // scroll movido para baixo
         if (winScrollCurrent > 0) {
@@ -41,14 +43,19 @@
                 
                 elHeader.classList.remove('header--show');
                 elHeader.classList.add('header--hidden');
-                // console.log('incrementando before', winScrollBefore);
+
+                elFooter.classList.remove('footer--hidden');
+                elFooter.classList.add('footer--show');
             }
-            // verifica se p scroll começou a ir para cima
+            // verifica se o scroll começou a ir para cima
             else {
                 winScrollBefore = winScrollCurrent;
 
                 elHeader.classList.remove('header--hidden');
                 elHeader.classList.add('header--show');
+
+                elFooter.classList.remove('footer--show');
+                elFooter.classList.add('footer--hidden');
             }
         }
         // scroll '0' no topo da página
@@ -56,6 +63,16 @@
             elHeader.classList.remove('header--hidden');
             elHeader.classList.add('header--show');
         };
+
+        // verifica se a página chegou ao final
+        if (docHeight == winHeight + winScrollCurrent) {
+            elCopyFooter.classList.remove('footer__copy--hidden');
+            elCopyFooter.classList.add('footer__copy--show');
+
+        } else {
+            elCopyFooter.classList.remove('footer__copy--show');
+            elCopyFooter.classList.add('footer__copy--hidden');
+        }
     })
 }());
 
