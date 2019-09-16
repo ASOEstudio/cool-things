17493df4d2678674cf2e6e03b9ada20687e1bcc9
Albert Souza <34682279+ASOEstudio@users.noreply.github.com>
2019-09-16 14:23:00 -0300

    criando listener no scroll

--- in-scroll-hide-toolbar/scripts.js
+++ in-scroll-hide-toolbar/scripts.js
@@ -1,10 +1,35 @@
 (function() {
+    'use strict';
+
+    // setando elementos no DOM
     var elHeader = document.querySelector('header'),
         elMain = document.querySelector('main');
+
+    // sai da função se o elemento não existir
+    if(!elHeader) return true;
 
     elMain.style.top = elHeader.clientHeight;
     
     console.log('altura do header', elHeader.clientHeight);
     console.log('top main', elMain.style.top);
+
+    // setando variáveis para scroll
+    var docHeight = 0,
+        winHeight = 0,
+        winScrollCurrent = 0;
+        // winScrollDiff = 0,
+        // winScrollBefore = 0;
+
+    window.addEventListener('scroll', function() {
+
+        // setando elementos no DOM
+        docHeight = document.body.offsetHeight;
+        winHeight = window.innerHeight;
+        winScrollCurrent = window.pageYOffset;
+        // winScrollDiff = winScrollBefore - winScrollCurrent;
+
+        console.log('docHeight', docHeight, 'winHeight', winHeight,
+            'winScrollCurrent', winScrollCurrent); //, 'winScrollDiff', winScrollDiff
+    })
 
 }());
