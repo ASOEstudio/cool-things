74bc3c704c5ee328aeae932c0e5a4276f57a853e
Albert Souza <34682279+ASOEstudio@users.noreply.github.com>
2019-09-19 15:20:45 -0300

    arrumando o código

--- http-request/scripts-Fetch.js
+++ http-request/scripts-Fetch.js
@@ -1,15 +1,15 @@
 'use strict'
 
 var myHeaders = new Headers({
-    "Content-Type": "text/plain",
-    "Content-Length": content.length.toString(),
-    "X-Custom-Header": "ProcessThisImmediately",
+    'Content-Type': 'text/xml',
+    'Content-Length': content.length.toString(),
+    'X-Custom-Header': 'ProcessThisImmediately',
 });
 
 function loadFetch() {
     fetch('cd_catalog.xml',
-        { method: 'GET', headers: myHeaders, mode: 'cors', cache: 'default' })
-    .then(res => {
+        { method: 'GET', headers: myHeaders, mode: 'cors', cache: 'default' }
+    ).then(res => {
         // verifica se o retorno está OK
         if (res.ok) {
             console.log('sem problema', res)
