9af1b3af4261758b2c952af4a08254549aea9def
Albert Souza <34682279+ASOEstudio@users.noreply.github.com>
2019-09-20 11:51:44 -0300

    testando fetch com json

--- http-request/scripts-Fetch.js
+++ http-request/scripts-Fetch.js
@@ -1,6 +1,7 @@
 'use strict'
 
-var myHeaders = new Headers({
+var secEl = document.getElementsByTagName('section')[0],
+    myHeaders = new Headers({
     'Content-Type': 'text/xml',
     'Content-Length': content.length.toString(),
     'X-Custom-Header': 'ProcessThisImmediately',
@@ -9,10 +10,10 @@ var myHeaders = new Headers({
 function loadFetch() {
     fetch('cd_catalog.xml',
         { method: 'GET', headers: myHeaders, mode: 'cors', cache: 'default' }
-    ).then(res => {
+    )
+    .then(res => {
         // verifica se o retorno está OK
         if (res.ok) {
-            console.log('sem problema', res)
             res.text()
             .then(
                 res => console.log(res),
@@ -22,4 +23,27 @@ function loadFetch() {
 
         } else console.log('com problema, erro', res.status.toString());
     })
+    .catch(err => {
+        console.log('Fetch Error :-S', err);
+    });
+}
+
+function loadFetchJson() {
+    fetch('json_mock.json',
+        { method: 'GET', headers: myHeaders, mode: 'cors', cache: 'default' }
+    )
+    .then(res => {
+        // verifica se o retorno está OK
+        if (res.ok) res.json().then(data => handler(data))
+        else console.log('com problema, erro', res.status.toString());
+    })
+    .catch(err => {
+        console.log('Fetch Error :-S', err);
+    });
+}
+
+function handler(res) {
+    let data = res.AvailabilitySummary.AvailabilitySummaryCarrier;
+
+    console.log(data);
 }
